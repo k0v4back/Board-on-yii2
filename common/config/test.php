be47b007416b7fb6c312ab42b83e11d8a3f09d13
Kosolapov Vadim <vadkos33@outlook.com>
2018-11-11 16:39:38 +0200

    Setup codecept

--- common/config/test.php
+++ common/config/test.php
@@ -7,8 +7,16 @@ return [
             'class' => 'yii\web\User',
             'identityClass' => 'common\models\User',
         ],
+//        'request' => [
+//            'class' => '\yii\web\Request',
+//            'cookieValidationKey' => 'K5TG_r4lh-XFNqbuPckY-bGiwDEDCpJ9',
+////            'class' => '\yii\web\Request',
+////            'enableCookieValidation' => false,
+////            'class' => '\yii\web\Request',
+////            'enableCookieValidation' => false,
+//        ],
         'request' => [
-            'cookieValidationKey' => 'test',
+            'cookieValidationKey' => new \yii\helpers\UnsetArrayValue(),
         ],
     ],
 ];
