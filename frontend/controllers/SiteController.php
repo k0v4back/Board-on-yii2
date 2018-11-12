59d57311377d680fb060adb2b62633a6c05370de
Kosolapov Vadim <vadkos33@outlook.com>
2018-11-12 19:24:42 +0200

    Added signup service

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace frontend\controllers;
 
+use frontend\services\users\SignupService;
 use Yii;
 use yii\base\InvalidParamException;
 use yii\web\BadRequestHttpException;
@@ -150,17 +152,18 @@ class SiteController extends Controller
      */
     public function actionSignup()
     {
-        $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post())) {
-            if ($user = $model->signup()) {
-                if (Yii::$app->getUser()->login($user)) {
-                    return $this->goHome();
-                }
+        $form = new SignupForm();
+        if ($form->load(Yii::$app->request->post())) {
+
+            $user = (new SignupService())->signup($form);
+
+            if (Yii::$app->getUser()->login($user)) {
+                return $this->goHome();
             }
         }
 
         return $this->render('signup', [
-            'model' => $model,
+            'model' => $form,
         ]);
     }
 
--- frontend/forms/SignupForm.php
+++ frontend/forms/SignupForm.php
@@ -2,21 +2,13 @@
 namespace frontend\forms;
 
 use yii\base\Model;
-use common\entities\User;
 
-/**
- * Signup form
- */
 class SignupForm extends Model
 {
     public $username;
     public $email;
     public $password;
 
-
-    /**
-     * {@inheritdoc}
-     */
     public function rules()
     {
         return [
@@ -34,21 +26,5 @@ class SignupForm extends Model
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
         ];
-    }
-
-    /**
-     * Signs user up.
-     *
-     * @return User|null the saved model or null if saving fails
-     */
-    public function signup()
-    {
-        if (!$this->validate()) {
-            return null;
-        }
-
-        $user = User::signup($this->username, $this->email, $this->password);
-        
-        return $user->save() ? $user : null;
     }
 }
