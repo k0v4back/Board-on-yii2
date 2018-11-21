007d77dfcbcb8b03394cb9771fd60758e7621a88
Kosolapov Vadim <vadkos33@outlook.com>
2018-11-21 20:48:18 +0200

    Added some design

--- frontend/views/cabinet/profile/index.php
+++ frontend/views/cabinet/profile/index.php
@@ -38,5 +38,174 @@ if (time() > $model->phone_verified_token_expire && $model->phone_verified_token
 ?>
 
 
+<style>
+    .load {
+        margin: 20px;
+    }
+
+    .data-user {
+        margin-top: 10px;
+    }
+
+</style>
+
+
+<div class="row">
+    <div class="col-lg-2 col-md-6 col-sm-6">
+        <img src="http://placehold.it/180x180" alt="140x140" class="img-rounded"><button class="btn btn-primary load">Загрузиить фото</button>
+    </div>
+    <div class="col-lg-4 col-md-6 col-sm-6 data-user">
+        <p>Имя: <?= Yii::$app->user->identity->username; ?></p>
+        <p>Фамилия: <?= Yii::$app->user->identity->last_name; ?></p>
+        <p>Телефон: <?= $model->phone; ?> <span class="glyphicon glyphicon-ok" aria-hidden="true" style="color: #00AF6F;" title="Подтверждён"></span>
+            <span class="glyphicon glyphicon-remove" aria-hidden="true" title="Не Подтверждён" style="color: #FF1E00;"></span> </p>
+        <a href="<?= \yii\helpers\Url::to(['cabinet/profile/edit']) ?>" class="btn btn-default" style="margin-bottom: 10px">Редактировать профиль</a>
+        <a href="<?= \yii\helpers\Url::to(['cabinet/profile/edit-phone']) ?>" class="btn btn-default">Редактировать телефон</a>
+    </div>
+</div>
+<hr><br>
+
+
+
+
+
+
+
+
+
+<!-- Title -->
+<div class="row">
+    <div class="col-xs-12">
+        <h4>Активные объявления | Завершённые объвления</h4>
+    </div>
+</div>
+
+<div class="row">
+
+    <!-- Post Content Column -->
+    <div class="col-lg-12">
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 hero-feature">
+            <div class="thumbnail">
+                <img src="http://placehold.it/800x500" alt="">
+                <div class="caption">
+                    <h3>Feature Label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <p>
+                        <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
